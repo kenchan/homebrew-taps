8bde7da20d0d7cd0057dded263f3b31dc0ce10db
Kenichi TAKAHASHI <kenichi.taka@gmail.com>
2016-12-09 17:42:11 +0900

    oops

--- mruby-cli.rb
+++ mruby-cli.rb
@@ -1,10 +1,8 @@
-MrubyCli::VERSION = '0.0.4'
-
 class MrubyCli < Formula
   desc "A utility for setting up a CLI with mruby that compiles binaries to Linux, OS X, and Windows."
   homepage "https://github.com/hone/mruby-cli"
-  url "https://github.com/hone/mruby-cli/releases/download/#{MrubyCli::VERSION}/mruby-cli-#{MrubyCli::VERSION}-x86_64-apple-darwin14.tgz"
-  version MrubyCli::VERSION
+  url "https://github.com/hone/mruby-cli/releases/download/0.0.4/mruby-cli-0.0.4-x86_64-apple-darwin14.tgz"
+  version '0.0.4'
   sha256 "e78be1c039085d0024fd8337ec40328cb1d64b7251357ec5ae8c680d9b27de8d"
 
   def install
